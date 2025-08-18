4d2f71d5ce502c2f795e626436d484a2cf651597
dhiyamnh <dhiya.dell@gmail.com>
2025-08-18 15:16:38 +0800

    edited the home & about iice

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -475,54 +475,46 @@
 
 
 
-    <!-- Footer Start -->
+    <!-- Footer Start (Full-Width) -->
     <div class="container-fluid bg-dark text-light footer my-6 mb-0 py-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container-fluid py-5">
-            <div class="row g-5 px-5">
-                
-                <!-- Office Address -->
-                <div class="col-lg-5 col-md-6">
-                    <h4 class="text-light mb-4">Office Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Institute of Informatics and Computing in Energy,<br>
-                        Universiti Tenaga Nasional,<br>
-                        Jalan IKRAM-UNITEN,<br>
-                        43000 Kajang,<br>
-                        Selangor, Malaysia</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+6 (0) 3 8921 7370 (Director)</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>iice@uniten.edu.my</p>
+        <div class="row g-5 px-5"> <!-- Added px-5 for inner padding -->
+            
+            <!-- Office Address -->
+            <div class="col-lg-5 col-md-6">
+                <h4 class="text-light mb-4">Office Address</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Institute of Informatics and Computing in Energy,<br>
+                    Universiti Tenaga Nasional,<br>
+                    Jalan IKRAM-UNITEN,<br>
+                    43000 Kajang,<br>
+                    Selangor, Malaysia</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+6 (0) 3 8921 7370 (Director)</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>iice@uniten.edu.my</p>
 
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-instagram"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-square btn-outline-light rounded-circle me-1" href=""><i class="fab fa-linkedin-in"></i></a>
                 </div>
+            </div>
 
-                <!-- Quick Links -->
-                <div class="col-lg-3 col-md-6 ms-auto">
-                    <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Our Services</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">Support</a>
-                </div>
-
+            <!-- Quick Links -->
+            <div class="col-lg-3 col-md-6 ms-auto">
+                <h4 class="text-light mb-4">Quick Links</h4>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Our Services</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">Support</a>
             </div>
         </div>
     </div>
     <!-- Footer End -->
 
-
-
-
-    <!-- Copyright Start -->
+    <!-- Copyright Start (Full-Width) -->
     <div class="container-fluid copyright text-light py-4 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a href="index.html">IICE</a>, All Right Reserved.
-                </div>
+        <div class="row px-5"> <!-- Added px-5 for inner padding -->
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                &copy; <a href="index.html">IICE</a>, All Right Reserved.
             </div>
         </div>
     </div>
@@ -530,10 +522,10 @@
 
 
     <!-- Back to Top -->
-    <a href="#" 
+    <!-- <a href="#" 
     class="btn btn-lg btn-primary rounded-circle back-to-top d-flex align-items-center justify-content-center">
     <i class="bi bi-arrow-up"></i>
-    </a>
+    </a> -->
 
 
     <!-- JavaScript Libraries -->
