b915fd3ede133f4ec2610292503eab1b1eac1699
nisah14 <23ftt1151@student.pb.edu.bn>
2025-08-20 11:00:37 +0800

    admin user added using laravel

---
+++ database/migrations/2025_08_20_021741_add_role_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add role column, default is 'member'
+            $table->string('role')->default('member')->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Remove role column if we rollback
+            $table->dropColumn('role');
+        });
+    }
+};
